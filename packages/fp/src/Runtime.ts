d0c5429fd6e26e4cc5568844696259bfcef59808
Conner Ruhl <connerruhl@me.com>
2020-03-25 13:32:23 -0500

    IO => Runtime renaming

--- packages/fp/src/Runtime.ts
+++ packages/fp/src/Runtime.ts
@@ -1,32 +1,37 @@
-import { Either, flow, identity, JSON, List, Option, pipe, IO } from ".";
+import { Either, flow, identity, JSON, List, Option, pipe, Runtime } from ".";
 
 export * from "io-ts";
 export * from "fp-ts/lib/IO";
 
 export type ToType<
-  Definition extends (() => IO.Any) | IO.Any
-> = Definition extends () => IO.Any
-  ? IO.TypeOf<ReturnType<Definition>>
-  : Definition extends IO.Any
-  ? IO.TypeOf<Definition>
+  Definition extends (() => Runtime.Any) | Runtime.Any
+> = Definition extends () => Runtime.Any
+  ? Runtime.TypeOf<ReturnType<Definition>>
+  : Definition extends Runtime.Any
+  ? Runtime.TypeOf<Definition>
   : never;
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const nullable = <Definition extends IO.Any>(definition: Definition) =>
-  IO.union([definition, IO.null, IO.undefined]);
+export const nullable = <Definition extends Runtime.Any>(
+  definition: Definition
+) => Runtime.union([definition, Runtime.null, Runtime.undefined]);
 
 export const fromPredicate = <A>(
   name: string,
   predicate: (a: unknown) => a is A
-): IO.Type<A> =>
-  new IO.Type(
+): Runtime.Type<A> =>
+  new Runtime.Type(
     name,
     predicate,
-    (a, context) => (predicate(a) ? IO.success(a) : IO.failure(a, context)),
+    (a, context) =>
+      predicate(a) ? Runtime.success(a) : Runtime.failure(a, context),
     identity
   );
 
-export const decode = <Definition extends IO.Any, A extends ToType<Definition>>(
+export const decode = <
+  Definition extends Runtime.Any,
+  A extends ToType<Definition>
+>(
   definition: Definition
 ) => (value?: Option.Nullable<unknown>): Either.ErrorOr<A> =>
   pipe(
@@ -39,7 +44,7 @@ export const decode = <Definition extends IO.Any, A extends ToType<Definition>>(
   );
 
 export const errorMessage = (
-  error: IO.ValidationError
+  error: Runtime.ValidationError
 ): Option.Option<string> =>
   pipe(
     List.head(error.context),
