5887f564a9b697824b3d492173b0d43a47d07c59
Conner Ruhl <connerruhl@me.com>
2020-04-12 23:03:52 -0500

    Rename

--- packages/fp/src/TaskEither.ts
+++ packages/fp/src/TaskEither.ts
@@ -4,5 +4,5 @@ export * from "fp-ts/lib/TaskEither";
 
 export type ErrorOr<A, L extends Error = Error> = TaskEither.TaskEither<L, A>;
 
-export const fromTry = <A>(fn: Fn.Lazy<Promise<A>>): ErrorOr<A> =>
+export const fromUnsafe = <A>(fn: Fn.Lazy<Promise<A>>): ErrorOr<A> =>
   TaskEither.tryCatch(fn, Exception.fromUnknown);
