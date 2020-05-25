37d6d44a47c42d7386b15cdb47d569cd1c4db3a9
Todd Elvers <todd.elvers@equipmentshare.com>
2020-05-25 13:02:14 -0500

    Corrected some documentation

--- packages/fp/src/Do.ts
+++ packages/fp/src/Do.ts
@@ -10,7 +10,7 @@ import { Do } from "fp-ts-contrib/lib/Do";
  * that threaded scope so that a custom value derived from the threaded scope can be returned.
  *
  * @example
- * const x = Result.Do()                    // Initiates do notation syntax
+ * const x = Either.do                      // Initiates do notation syntax
  *   .bind('a', 10)                         // Eagerly set 'a' to 10 in the context
  *   .bindL('b', () => 20)                  // Lazily set 'b' 20 in the context
  *   .bindL('c', ({ a, b }) => b + a)       // Lazily calculate 'c' from 'a' and 'b' in the context
@@ -18,18 +18,12 @@ import { Do } from "fp-ts-contrib/lib/Do";
  *
  * const y = pipe(
  *   x,
- *   Result.getOrElse(
+ *   Either.getOrElse(
  *     () => "No dollars"
  *   )
  * )
  *
  * assert.deepStrictEqual(y, `30 dollars`)
- *
- * All `do` operations mimic the execute-on-success/ignore-on-failure behavior that functions
- * like `Result.andThen` and `Result.map` exhibit. This is where the benefits of do notation
- * lie: it allows you to concisely describe a series of instructions by only concerning yourself
- * with what should happen in the successful scenario.  In the above example if any step of the
- * process failed, `y` would end up being set to "No dollars".
  */
 export const forEither = Do(Either.either);
 
@@ -41,7 +35,7 @@ export const forEither = Do(Either.either);
  * that threaded scope so that a custom value derived from the threaded scope can be returned.
  *
  * @example
- * const x = Result.Async.Do()              // Initiates do notation syntax
+ * const x = TaskEither.do                  // Initiates do notation syntax
  *   .bind('a', 10)                         // Eagerly set 'a' to 10 in the context
  *   .bindL('b', () => 20)                  // Lazily set 'b' 20 in the context
  *   .bindL('c', ({ a, b }) => b + a)       // Lazily calculate 'c' from 'a' and 'b' in the context
@@ -49,17 +43,11 @@ export const forEither = Do(Either.either);
  *
  * const y = pipe(
  *   x,
- *   Result.Async.getOrElse(
+ *   TaskEither.getOrElse(
  *     () => "No dollars"
  *   )
  * )
  *
  * assert.deepStrictEqual(y, `30 dollars`)
- *
- * All `do` operations mimic the execute-on-success/ignore-on-failure behavior that functions
- * like `Result.Async.andThen` and `Result.Async.map` exhibit. This is where the benefits of
- * do notation lie: it allows you to concisely describe a series of instructions by only
- * concerning yourself with what should happen in the happy path.  In the above example if
- * any step of the process failed, `y` would end up being set to "No dollars".
  */
 export const forTaskEither = Do(TaskEither.taskEither);
