79b15c04926a2ed3965661b273640415e0971a6e
Conner Ruhl <conner.ruhl@equipmentshare.com>
2020-05-25 14:46:53 -0500

    Removed newline

--- packages/fp/src/Exception.ts
+++ packages/fp/src/Exception.ts
@@ -2,5 +2,5 @@ import { JSON, Option } from ".";
 
 export const fromUnknown = (unknown?: Option.Nullable<unknown>): Error =>
   !(unknown instanceof Error)
-    ? new Error(`Unknown error...\n\n${JSON.Stringify.Always.pretty(unknown)}`)
+    ? new Error(`Unknown error...\n${JSON.Stringify.Always.pretty(unknown)}`)
     : unknown;
