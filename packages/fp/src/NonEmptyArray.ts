9996ab27e5a74920a9871466f0eb49f9cab3eec5
Conner Ruhl <conner.ruhl@equipmentshare.com>
2020-06-24 10:44:19 -0500

    Fixed issue with imports being undefined at runtime

--- packages/fp/src/NonEmptyArray.ts
+++ packages/fp/src/NonEmptyArray.ts
@@ -2,8 +2,6 @@ import { NonEmptyArray, Option } from ".";
 
 export * from "fp-ts/lib/NonEmptyArray";
 
-export const fromArray: <A>(
+export const fromArray = <A>(
   as: A[] | readonly A[]
-) => Option.Option<
-  NonEmptyArray.NonEmptyArray<A>
-> = NonEmptyArray.fromArray as any;
+): Option.Option<NonEmptyArray.NonEmptyArray<A>> => NonEmptyArray.fromArray(as);
